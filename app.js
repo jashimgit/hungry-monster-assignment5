34f7631bd7014dbe44044e00852333006d75aeec
jashimgit <jashim.ncn@gmail.com>
2021-02-07 02:48:36 +0600

    meal dynamic id added in showMealDetails function

--- app.js
+++ app.js
@@ -3,49 +3,35 @@ const mealsContainer = document.getElementById('meals-container');
 function getFoods() {
     const searchQuery = document.getElementById('foodName').value;
     const query = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchQuery}`;
-    // console.log(query);
+
     fetch(query)
         .then(res => res.json())
         .then(data => getFood(data))
 
 }
 
-function getFood(data) {
+const getFood = data => {
     let food = '';
-    console.log(data.meals[0].strMeal);
     for (let i = 0; i < data.meals.length; i++) {
         food +=
-            `<div class="col-sm-3">
-             <div class="card">
+            `<div class="col-sm-3 mb-3">
+                <div class="card">
                 <img src="${data.meals[i].strMealThumb}" class="card-img-top">
-                     <div class=" card-body">
-                         <h5 class="card-title">${data.meals[i].strMeal}</h5>
-                         <a href="#" class="btn btn-primary btn-sm">Details</a>
-                      </div>
-               </div> 
-         </div>`;
+                    <div class=" card-body">
+                        <h5 class="card-title">${data.meals[i].strMeal}</h5>
+                        <button href="#"  onclick="showMealDetails('${data.meals[i].idMeal}');" class="btn btn-primary btn-sm">Details</button>
+                    </div>
+                </div> 
+            </div>`;
     }
 
     mealsContainer.innerHTML = food;
-    // for (let i = 0; i < foods.length; i++) {
-    //     const food = foods[i];
-    //     console.log(food);
-    //     const foodInfo =
-    //         `<div class="col-sm-3">
-    //         <div class="card">
-    //             <img src="${food[i].meals[0].strMealThumb}" class="card-img-top">
-    //                 <div class=" card-body">
-    //                     <h5 class="card-title">${food[i].meals[0].strMeal}</h5>
-    //                     <a href="#" class="btn btn-primary btn-sm">Details</a>
-    //                  </div>
-    //           </div> 
-    //     </div>`;
-    //     mealsContainer.innerHTML = foodInfo;
-    // }
-    /*
-    console.log(foods.meals[0].strMeal);
-    console.log(foods.meals[0].idMeal);
-    console.log(foods.meals[0].strMealThumb);
-*/
+}
 
+const showMealDetails = meal => {
+    const mealUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal}`
+    // console.log(mealUrl);
+    fetch(mealUrl)
+        .then(res => res.json())
+        .then(mealData => console.log(mealData))
 }
